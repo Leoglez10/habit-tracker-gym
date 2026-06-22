aa0f047e1347daa7a7f7649052f3e62cee85ae92
Leoglez10 <leonardo.gonzalez9264@alumnos.udg.mx>
2026-06-22 16:21:33 -0600

    primer push de la app

---
+++ sw.js
@@ -0,0 +1,38 @@
+// Service worker mínimo: cachea la app para que funcione sin internet.
+const CACHE = 'disciplina-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Estrategia "network-first" para el HTML (para recibir actualizaciones),
+// y "cache-first" para el resto. Si no hay red, tira del cache.
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match('./index.html')))
+  );
+});
